9ae8a8bbdb95544768385f6f03669a8f768adc0d
Mai Eisenberg <r0377155@student.thomasmore.be>
2024-12-18 22:17:19 +0100

    fix: navAdmin styling

--- navAdmin.inc.php
+++ navAdmin.inc.php
@@ -1,17 +1,17 @@
- <div class="hamburger">
-      <div class="line"></div>
-      <div class="line"></div>
-      <div class="line"></div>
+<nav class="navbarUser">
+    <div class="logoNav" >
+    <a href="index.php" class="logo"><img src="./assets/HOM.png" alt="logo"></a>
     </div>
 
-<nav class="navbar">
-    <a href="index.php" class="logo">Home</a>
-    <a href="index.php">Shop</a>
+    <div class="btnNav">
+      <a class="btnsNav" href="index.php">Shop</a>
+      <a class="btnsNav" href="ourTeam.php">Our team</a>
+      <a class="btnsNav" href="mylist.php">My admin</a>
+      <a class="btnsNav" href="addProduct.php">Add product</a>
+    </div>
     
-    <form action="" method="get">
-      <input type="text" name="search">
-    </form>
+    <div class="restNav">
+      <a href="logout.php" class="navbar__logout">Logout</a>
+    </div>
     
-    <a href="addProduct.php">Add product</a>
-    <a href="logout.php" class="navbar__logout">Logout</a>
 </nav>
