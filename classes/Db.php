9f19a4f94c73b0f3d210c8e57fa7edbcebeb4358
Mai Eisenberg <r0377155@student.thomasmore.be>
2025-01-08 17:28:28 +0200

    fix:root railway

--- classes/Db.php
+++ classes/Db.php
@@ -2,16 +2,30 @@
 error_reporting(E_ALL);
 
 class Db {
-    private static $conn;
-    public static function getConnection() {
-        if (self::$conn == null) {
-            try {
-                self::$conn = new PDO('mysql:host=localhost;dbname=houseofmoose', 'root', '');
-                self::$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            } catch (PDOException $e) {
-                die("Database connection failed: " . $e->getMessage());
-            }
+    private $host = 'junction.proxy.rlwy.net';
+    private $user = 'root';
+    private $pass = 'bqemkIoAJAMucwKIgVOPPkanONSUNOoZ';
+    private $dbname = 'railway';
+    private $port = '40235';
+
+    public $conn;
+
+    public function connect() {
+        $this->conn= new mysqli($this->host, $this->user, $this->pass, $this->dbname, $this->port);
+        if ($this->conn->connect_error) {
+            die("Connection failed: " . $this->conn->connect_error);
         }
-        return self::$conn;
+        return $this->conn;
     }
+    // public static function getConnection() {
+    //     if (self::$conn == null) {
+    //         try {
+    //             self::$conn = new PDO('mysql:host=localhost;dbname=houseofmoose', 'root', '');
+    //             self::$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //         } catch (PDOException $e) {
+    //             die("Database connection failed: " . $e->getMessage());
+    //         }
+    //     }
+    //     return self::$conn;
+    // }
 }
