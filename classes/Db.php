4be908ce3cbd07d2f4656208725b5beb6bec657b
Mai Eisenberg <r0377155@student.thomasmore.be>
2025-01-08 17:32:15 +0200

    refactor: rename connect method to getConnection

--- classes/Db.php
+++ classes/Db.php
@@ -10,7 +10,7 @@ class Db {
 
     public $conn;
 
-    public function connect() {
+    public function getConnection() {
         $this->conn= new mysqli($this->host, $this->user, $this->pass, $this->dbname, $this->port);
         if ($this->conn->connect_error) {
             die("Connection failed: " . $this->conn->connect_error);
